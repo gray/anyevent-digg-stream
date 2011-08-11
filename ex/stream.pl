494e342efd24ccc20c3db21f9b2cf3834528739e
gray <gray@cpan.org>
2011-08-11 05:35:51 -0700

    add timestamp to message

--- ex/stream.pl
+++ ex/stream.pl
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use AnyEvent;
 use AnyEvent::Digg::Stream;
+use POSIX qw(strftime);
 
 binmode STDOUT, ":utf8";
 
@@ -11,7 +12,8 @@ my $cv = AnyEvent->condvar;
 my $client = AnyEvent::Digg::Stream->new(
     on_event => sub {
         my $event = shift;
-        printf "%s for %s\n", $event->{type}, $event->{item}{link};
+        printf "[%s] %s for %s\n", strftime('%F %T', localtime),
+            $event->{type}, $event->{item}{link};
     },
 );
 
